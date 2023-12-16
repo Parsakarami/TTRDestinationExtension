fb68fbe2578cc63355bb299c1da4abe0370e4bc7
Parsa Karami <Parsakarami@live.com>
2023-12-16 18:01:03 -0500

    Add TTR project

--- TTRDestinationExtension/ContentView.swift
+++ TTRDestinationExtension/ContentView.swift
@@ -13,30 +13,47 @@ struct ContentView: View {
     @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
+        ZStack(content:{
+//            NavigationSplitView {
+//                LinearGradient(gradient: Gradient(colors:[.green, .blue]),
+//                               startPoint: .topLeading, endPoint: .bottomTrailing)
+//                .edgesIgnoringSafeArea(.all)
+//                
+//                List {
+//                    ForEach(items) { item in
+//                        NavigationLink {
+//                            Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+//                        } label: {
+//                            Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+//                        }
+//                    }
+//                    .onDelete(perform: deleteItems)
+//                }
+//                .toolbar {
+//                    ToolbarItem(placement: .navigationBarTrailing) {
+//                        EditButton()
+//                    }
+//                    ToolbarItem {
+//                        Button(action: addItem) {
+//                            Label("Add Item", systemImage: "plus")
+//                        }
+//                    }
+//                }
+//            } detail: {
+//                Text("Select an item")
+//            }
+            
+            GradientBackground(topColor: .blue, bottomColor: .black)
+            
+            HStack(alignment: .bottom, spacing: 10, content: {
+                VStack(alignment: .trailing, content: {
+                    CustomButton(text: "User 1", systemImage: "plus", function: addItem)
+                    CustomButton(text: "User 2", systemImage: "plus", function: addItem)
+                    CustomButton(text: "User 3", systemImage: "plus", function: addItem)
+                    })
+            }).frame(alignment: .trailing)
+            
+        })
     }
 
     private func addItem() {
@@ -58,4 +75,29 @@ struct ContentView: View {
 #Preview {
     ContentView()
         .modelContainer(for: Item.self, inMemory: true)
+}
+
+struct GradientBackground: View {
+    var topColor: Color
+    var bottomColor: Color
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors:[topColor,bottomColor]),
+                       startPoint: .topLeading, endPoint: .bottomTrailing)
+                       .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CustomButton : View {
+    var text: String
+    var systemImage: String
+    var function : () -> Void
+    var body: some View {
+        Button(action: function) {
+            Label(text, systemImage: systemImage).foregroundColor(.blue)
+        }
+        .frame(width: 200,height: 30)
+        .padding(10)
+        .background(.white)
+        .cornerRadius(4.5)
+    }
 }
